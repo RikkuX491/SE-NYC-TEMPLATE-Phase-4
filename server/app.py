9906694b1bc0bde3374b0323fe542dc526e9fec5
Ricardo Guerra <RikkuG491@gmail.com>
2024-10-30 18:28:05 -0400

    Updated lecture content

--- server/app.py
+++ server/app.py
@@ -20,25 +20,21 @@ migrate = Migrate(app, db)
 # initialize the Flask application to use the database
 db.init_app(app)
 
-# GET all hotels with /hotels
 @app.route('/hotels')
 def all_hotels():
+    # This code should only be executed if the request method is "GET"
     hotels = Hotel.query.all()
     response_body = [hotel.to_dict(only=('id', 'name')) for hotel in hotels]
     return make_response(response_body, 200)
 
-# GET hotel by id with /hotels/<int:id>
 @app.route('/hotels/<int:id>')
 def hotel_by_id(id):
     hotel = db.session.get(Hotel, id)
 
     if hotel:
-        # response_body = hotel.to_dict(rules=('-reviews.hotel', '-reviews.customer.reviews'))
+        # This code should only be executed if the request method is "GET"
         response_body = hotel.to_dict(rules=('-reviews.hotel', '-reviews.customer'))
-
-        # Add in the association proxy data (The hotel's customers)
         response_body['customers'] = [customer.to_dict(only=('id', 'first_name', 'last_name')) for customer in hotel.customers]
-        
         return make_response(response_body, 200)
     else:
         response_body = {
@@ -46,24 +42,21 @@ def hotel_by_id(id):
         }
         return make_response(response_body, 404)
 
-# GET all customers with /customers
 @app.route('/customers')
 def all_customers():
+    # This code should only be executed if the request method is "GET"
     customers = Customer.query.all()
     customer_list_with_dictionaries = [customer.to_dict(only=('id', 'first_name', 'last_name')) for customer in customers]
     return make_response(customer_list_with_dictionaries, 200)
 
-# GET customer by id with /customers/<int:id>
 @app.route('/customers/<int:id>')
 def customer_by_id(id):
     customer = db.session.get(Customer, id)
 
     if customer:
+        # This code should only be executed if the request method is "GET"
         response_body = customer.to_dict(rules=('-reviews.hotel', '-reviews.customer'))
-
-        # Add in the association proxy data (The customer's hotels)
         response_body['hotels'] = [hotel.to_dict(only=('id', 'name')) for hotel in customer.hotels]
-
         return make_response(response_body, 200)
     else:
         response_body = {
@@ -71,19 +64,19 @@ def customer_by_id(id):
         }
         return make_response(response_body, 200)
 
-# GET all reviews with /reviews
 @app.route('/reviews')
 def all_reviews():
+    # This code should only be executed if the request method is "GET"
     reviews = Review.query.all()
     review_list_with_dictionaries = [review.to_dict(rules=('-hotel.reviews', '-customer.reviews')) for review in reviews]
     return make_response(review_list_with_dictionaries, 200)
 
-# GET review by id with /reviews/<int:id>
 @app.route('/reviews/<int:id>')
 def review_by_id(id):
     review = db.session.get(Review, id)
 
     if review:
+        # This code should only be executed if the request method is "GET"
         response_body = review.to_dict(rules=('-hotel.reviews', '-customer.reviews'))
         return make_response(response_body, 200)
     else:
