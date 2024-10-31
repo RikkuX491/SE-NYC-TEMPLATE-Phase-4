3b7b15995ec9553d838adb314b114ca1a99393bb
Ricardo Guerra <RikkuG491@gmail.com>
2024-10-31 17:48:48 -0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -2,15 +2,16 @@
 
 import ipdb
 
-# make_response() is a function from the flask library that returns a Response object. We can include data (a list, dictionary, or string) and a status code and pass these in as arguments to the make_response() function. We can return a Response object from a Flask view.
+# make_response() is a function from the flask library that returns a Response object. We can include data (a list, dictionary, or string) and a status code and pass these in as arguments to the make_response() function. We can return a Response object from a Flask view
 from flask import Flask, make_response
 
-# Migrate is a class from the flask_migrate library that creates a Migrate object that can be used to connect Flask-Migrate to your Flask app and database.
+# Migrate is a class from the flask_migrate library that creates a Migrate object that can be used to connect Flask-Migrate to your Flask app and database
 from flask_migrate import Migrate
 
 # db is a variable containing an instance of the SQLAlchemy class (Flask SQLAlchemy extension)
 from models import db
 
+# app contains our Flask app, which is an instance of the Flask class
 app = Flask(__name__)
 
 # configure a database connection to the local file hotels.db
